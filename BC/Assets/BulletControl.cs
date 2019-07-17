977c6306346800280c5038f5c4830e171223bb7b
Minui <minui.l@digipen.edu>
2019-07-17 14:46:38 -0700

    Unity Project

---
+++ BC/Assets/BulletControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+
+        if(coll.gameObject.tag == "LV4TANK" || coll.gameObject.tag == "WALL")
+        {
+            //coll.gameObject.GetComponent<TankHealth>().HP -= 1;
+        }
+        else if(coll.gameObject.tag == "WALL_normal")
+        {
+            Destroy(coll.gameObject);
+        }
+        GameObject[] playerTank = GameObject.FindGameObjectsWithTag("Player");
+
+        playerTank[0].GetComponent<PlayerFireControl>().IsBulletAlive = false;
+
+        Destroy(gameObject);
+    }
+}
---
+++ BC/Assets/PlayerFireControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFireControl : MonoBehaviour
+{
+    public float bulletSpeed = 10f;
+    public float firerate = 0.5f;
+    private float nextfire = 0.0f;
+
+    public Transform Gun;
+    public Rigidbody2D projectile;
+
+    private Vector3 FireDirection;
+
+    [HideInInspector]
+    public bool IsBulletAlive;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FireDirection = new Vector3(0, 1, 0);
+        IsBulletAlive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( (Input.GetKeyDown(KeyCode.Z) && Time.time > nextfire) && !IsBulletAlive)
+        {
+            nextfire = Time.time + firerate;
+            Fire();
+        }
+
+    }
+
+    void Fire()
+    {
+        Rigidbody2D bulletInstance = Instantiate(projectile, Gun.position, transform.rotation) as Rigidbody2D;
+        bulletInstance.velocity = transform.up * bulletSpeed;
+        IsBulletAlive = true;
+    }
+
+}
