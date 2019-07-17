5a5f40a59f2da3d015baa1b8bdeaefb11ff64442
CSK <c.kwon@digipen.edu>
2019-07-17 14:57:25 -0700

    GameController script

---
+++ BC/Assets/GameController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    //Game state manager.
+
+
+    //life +1
+    protected int life;      
+    
+    
+    //bomb
+    
+    //Freeze time
+
+    //shovel
+  
+    //Star
+    
+    //
+    
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+
+
+
+}
